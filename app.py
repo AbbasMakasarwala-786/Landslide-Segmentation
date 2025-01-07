f52461d459d6a5c6cccc328584c410adb846e8da
AbbasMakasarwala-786 <abbasmakasarwala1@gmail.com>
2025-01-07 22:13:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from PIL import Image
 import cv2
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.layers import Input,Conv2D,BatchNormalization,Activation,MaxPool2D,Conv2DTranspose,Concatenate
 from tensorflow.keras.models import Model
